1c750063626b5a140a664cb54e2cbc62f6eeb71d
Shivanand Kumar <itzshivanand@gmail.com>
2025-09-13 23:48:19 +0530

    some issues fix

--- backend/src/config/cookieOptions.js
+++ backend/src/config/cookieOptions.js
@@ -1,8 +1,9 @@
 const cookieOptions = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-  sameSite: "lax",
-  maxAge: 24 * 60 * 60 * 1000, // 1 day
+  secure: process.env.NODE_ENV === "production", 
+  sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
+  maxAge: 24 * 60 * 60 * 1000,
 };
+
 
 export default cookieOptions;
