39cadd3dc7b3709ed3199e23746b80c5e2ff2856
Shivanand Kumar <itzshivanand@gmail.com>
2025-09-13 23:21:25 +0530

    deployment testing

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api", 
+  baseURL: import.meta.env.VITE_API_URL || "https://lead-management-system-sage.vercel.app/api", 
   withCredentials: true, 
 });
 
