932563d73ce9a0f7ab5322fafd58f245cbb569b2
Shivanand Kumar <itzshivanand@gmail.com>
2025-09-13 23:57:16 +0530

    update cookiesoptions

--- backend/src/config/cookieOptions.js
+++ backend/src/config/cookieOptions.js
@@ -1,9 +1,9 @@
 const cookieOptions = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production", 
-  sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
+  secure: process.env.NODE_ENV === "production" ? true : false,
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   maxAge: 24 * 60 * 60 * 1000,
-  path: "/", 
+  path: "/",
 };
 
 
