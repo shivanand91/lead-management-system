2791cbb5643bbd0cbd7fbb205cb9ad3302c53f45
Shivanand Kumar <itzshivanand@gmail.com>
2025-09-13 23:50:15 +0530

    path added for every route

--- backend/src/config/cookieOptions.js
+++ backend/src/config/cookieOptions.js
@@ -3,6 +3,7 @@ const cookieOptions = {
   secure: process.env.NODE_ENV === "production", 
   sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
   maxAge: 24 * 60 * 60 * 1000,
+  path: "/", 
 };
 
 
